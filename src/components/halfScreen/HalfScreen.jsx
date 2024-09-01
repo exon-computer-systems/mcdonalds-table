89984240ee91323a20c5fc406a884cb1ff6cb257
Szymon <szmn.rtn@gmail.com>
2024-09-02 00:23:43 +0200

    Create mini screens and anim background

---
+++ src/components/halfScreen/HalfScreen.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HalfScreen = () => {
+    return <section className="half"></section>;
+};
+
+export default HalfScreen;
---
+++ src/components/quarterScreen/QuarterScreen.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const QuarterScreen = () => {
+    return <section className="quarter"></section>;
+};
+
+export default QuarterScreen;
