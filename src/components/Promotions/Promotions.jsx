cfdd1ba5d6dc727cad476629ea93b9879260a38f
Patryk Kulpa <patkul004@pbs.edu.pl>
2024-09-05 15:59:34 +0200

    fixed promo comp

--- src/components/Promotions/Promotions.jsx
+++ src/components/Promotions/Promotions.jsx
@@ -1,6 +1,8 @@
 import styles from "./Promotions.module.css";
 import img1 from "../../img/img1.jpg";
 import img2 from "../../img/img2.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
 const Promotions = () => {
   return (
@@ -10,9 +12,11 @@ const Promotions = () => {
       <section className={styles.scroll_container}>
         <h2>Burgery</h2>
         <section className={styles.scroll_wrapper}>
+          <FontAwesomeIcon icon={faAngleLeft} />
           {Array.from({ length: 10 }).map((_, index) => (
             <img src={img1} alt="i" key={index} />
           ))}
+          <FontAwesomeIcon icon={faAngleRight} />
         </section>
       </section>
       <section className={styles.scroll_container}>
