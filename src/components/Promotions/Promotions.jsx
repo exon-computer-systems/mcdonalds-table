1a21da572997322bb23e7b3495c03f8920af1107
Patryk Kulpa <patkul004@pbs.edu.pl>
2024-09-05 17:36:01 +0200

    added scroll func

--- src/components/Promotions/Promotions.jsx
+++ src/components/Promotions/Promotions.jsx
@@ -1,32 +1,13 @@
 import styles from "./Promotions.module.css";
-import img1 from "../../img/img1.jpg";
-import img2 from "../../img/img2.jpg";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+
+import PromoSlider from "./PromoSlider";
 
 const Promotions = () => {
   return (
     <section className={styles.container}>
       <h1 className={styles.heading}>Promocje</h1>
-
-      <section className={styles.scroll_container}>
-        <h2>Burgery</h2>
-        <section className={styles.scroll_wrapper}>
-          <FontAwesomeIcon icon={faAngleLeft} />
-          {Array.from({ length: 10 }).map((_, index) => (
-            <img src={img1} alt="i" key={index} />
-          ))}
-          <FontAwesomeIcon icon={faAngleRight} />
-        </section>
-      </section>
-      <section className={styles.scroll_container}>
-        <h2>Zestawy</h2>
-        <section className={styles.scroll_wrapper}>
-          {Array.from({ length: 10 }).map((_, index) => (
-            <img src={img2} alt="i" key={index} />
-          ))}
-        </section>
-      </section>
+      <PromoSlider />
+      <PromoSlider />
     </section>
   );
 };
--- src/components/halfScreen/HalfScreen.jsx
+++ src/components/halfScreen/HalfScreen.jsx
@@ -3,12 +3,12 @@ import styles from "./HalfScreen.module.css";
 import Menu from "../menu/Menu";
 import Promotions from "../promotions/Promotions";
 const HalfScreen = () => {
-    return (
-        <section className={styles.half}>
-            {/* <Menu /> */}
-            <Promotions />
-        </section>
-    );
+  return (
+    <section className={styles.half}>
+      {/* <Menu /> */}
+      <Promotions />
+    </section>
+  );
 };
 
 export default HalfScreen;
