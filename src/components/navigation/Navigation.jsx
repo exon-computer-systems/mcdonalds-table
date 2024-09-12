1ae7328138c481856aa8387db218dacec6b9481e
Patryk Kulpa <patkul004@pbs.edu.pl>
2024-09-12 13:21:08 +0200

    fixed nav

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -8,23 +8,24 @@ const Navigation = ({ switchComponent }) => {
   const [isClicked, setIsClicked] = useState(false);
   const handleClick = () => {
     setIsClicked(!isClicked);
+    console.log("clicked");
   };
 
   return (
-    <motion.section
-      className={styles.container}
-      initial={{ x: 0, y: 5, height: 10 }}
-      animate={
-        isClicked
-          ? {
-              x: 0,
-              y: 0,
-              height: 250,
-            }
-          : { x: 0, y: 0 }
-      }
-    >
-      <section className={styles.wrapper}>
+    <>
+      <motion.section
+        initial={{ x: 0, y: 0 }}
+        animate={
+          isClicked
+            ? {
+                x: 0,
+                y: -240,
+              }
+            : { x: 0, y: 0 }
+        }
+        transition={{ type: "tween" }}
+        className={styles.slide_nav}
+      >
         <motion.button
           onClick={handleClick}
           className={styles.icon_down}
@@ -33,21 +34,38 @@ const Navigation = ({ switchComponent }) => {
         >
           <FontAwesomeIcon icon={faAngleUp} />
         </motion.button>
-        <motion.section
-          animate={isClicked ? { opacity: 1 } : { opacity: 0 }}
-          className={styles.btns_wrapper}
-        >
-          <button onClick={() => switchComponent("shoppingCart")}>
-            Aplikacja
-          </button>
-          <button>Kelner</button>
-          <button onClick={() => switchComponent("promotions")}>
-            Promocje
-          </button>
-          <button onClick={() => switchComponent("menu")}>Menu</button>
-        </motion.section>
-      </section>
-    </motion.section>
+      </motion.section>
+      <motion.section
+        className={styles.container}
+        initial={{ x: 0, y: 0, height: 10, display: "none" }}
+        animate={
+          isClicked
+            ? {
+                x: 0,
+                y: 0,
+                height: 250,
+                display: "flex",
+              }
+            : { x: 0, y: 0 }
+        }
+      >
+        <section className={styles.wrapper}>
+          <motion.section
+            animate={isClicked ? { opacity: 1 } : { opacity: 0 }}
+            className={styles.btns_wrapper}
+          >
+            <button onClick={() => switchComponent("shoppingCart")}>
+              Aplikacja
+            </button>
+            <button>Kelner</button>
+            <button onClick={() => switchComponent("promotions")}>
+              Promocje
+            </button>
+            <button onClick={() => switchComponent("menu")}>Menu</button>
+          </motion.section>
+        </section>
+      </motion.section>
+    </>
   );
 };
 
