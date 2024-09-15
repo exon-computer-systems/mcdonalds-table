79b4399c4e70d4b06fb5fa3167c783ea63fabf17
Szymon <szmn.rtn@gmail.com>
2024-09-15 21:32:17 +0200

    Fix navigation routes

--- src/components/halfScreen/HalfScreen.jsx
+++ src/components/halfScreen/HalfScreen.jsx
@@ -8,39 +8,38 @@ import Navigation from "../navigation/Navigation";
 import Application from "../application/Application";
 import MemoryGame from "../memoryGame/MemoryGame";
 const HalfScreen = () => {
-  const [activeComponent, setActiveComponent] = useState("menu");
-  const switchComponent = componentName => {
-    switch (componentName) {
-      case "menu":
-        setActiveComponent("menu");
-        break;
-      case "game":
-        setActiveComponent("game");
-        break;
-      case "promotions":
-        setActiveComponent("promotions");
-        break;
-      case "messages":
-        setActiveComponent("messages");
-        break;
-      case "application":
-        setActiveComponent("application");
-        break;
-      default:
-        setActiveComponent("menu");
-    }
-  };
-  return (
-    <section className={styles.half}>
-      <Navigation switchComponent={switchComponent} />
-
-      {activeComponent === "menu" && <Menu />}
-      {activeComponent === "game" && <MemoryGame />}
-      {activeComponent === "promotions" && <Promotions />}
-      {activeComponent === "messages" && <Messages />}
-      {activeComponent === "application" && <Application />}
-    </section>
-  );
+    const [activeComponent, setActiveComponent] = useState("menu");
+    const switchComponent = (componentName) => {
+        switch (componentName) {
+            case "menu":
+                setActiveComponent("menu");
+                break;
+            case "promotions":
+                setActiveComponent("promotions");
+                break;
+            case "messages":
+                setActiveComponent("messages");
+                break;
+            case "application":
+                setActiveComponent("application");
+                break;
+            case "games":
+                setActiveComponent("games");
+                break;
+            default:
+                setActiveComponent("menu");
+        }
+    };
+    return (
+        <section className={styles.half}>
+            <Navigation switchComponent={switchComponent} />
+            {activeComponent === "menu" && <Menu />}
+            {activeComponent === "promotions" && <Promotions />}
+            {activeComponent === "messages" && <Messages />}
+            {activeComponent === "application" && <Application />}
+            {activeComponent === "games" && <MemoryGame />}
+        </section>
+    );
 };
 
 export default HalfScreen;
--- src/components/quarterScreen/QuarterScreen.jsx
+++ src/components/quarterScreen/QuarterScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./QuarterScreen.module.css";
 import Menu from "../menu/Menu";
 import Promotions from "../promotions/Promotions";
@@ -9,39 +9,40 @@ import Application from "../application/Application";
 import MemoryGame from "../memoryGame/MemoryGame";
 
 const QuarterScreen = () => {
-  const [activeComponent, setActiveComponent] = useState("menu");
-  const switchComponent = componentName => {
-    switch (componentName) {
-      case "menu":
-        setActiveComponent("menu");
-        break;
-      case "promotions":
-        setActiveComponent("promotions");
-        break;
-      case "messages":
-        setActiveComponent("messages");
-        break;
-      case "application":
-        setActiveComponent("application");
-        break;
-      case "game":
-        setActiveComponent("game");
-        break;
-      default:
-        setActiveComponent("menu");
-    }
-  };
-  return (
-    <section className={styles.quarter}>
-      <Navigation switchComponent={switchComponent} />
+    const [activeComponent, setActiveComponent] = useState("menu");
+    const switchComponent = (componentName) => {
+        switch (componentName) {
+            case "menu":
+                setActiveComponent("menu");
+                break;
+            case "promotions":
+                setActiveComponent("promotions");
+                break;
+            case "messages":
+                setActiveComponent("messages");
+                break;
+            case "application":
+                setActiveComponent("application");
+                break;
+            case "games":
+                setActiveComponent("games");
+                break;
+            default:
+                setActiveComponent("menu");
+        }
+    };
 
-      {activeComponent === "menu" && <Menu />}
-      {activeComponent === "promotions" && <Promotions />}
-      {activeComponent === "messages" && <Messages />}
-      {activeComponent === "application" && <Application />}
-      {activeComponent === "game" && <MemoryGame />}
-    </section>
-  );
+    return (
+        <section className={styles.quarter}>
+            <Navigation switchComponent={switchComponent} />
+
+            {activeComponent === "menu" && <Menu />}
+            {activeComponent === "promotions" && <Promotions />}
+            {activeComponent === "messages" && <Messages />}
+            {activeComponent === "application" && <Application />}
+            {activeComponent === "games" && <MemoryGame />}
+        </section>
+    );
 };
 
 export default QuarterScreen;
