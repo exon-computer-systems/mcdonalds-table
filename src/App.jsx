687cb573c89abb883384affe163d2ee351dc414a
Patryk Kulpa <patkul004@pbs.edu.pl>
2024-10-13 15:58:01 +0200

    added alerts and style adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -4,208 +4,206 @@ import QuarterScreen from "./components/quarterScreen/QuarterScreen";
 import WelcomePage from "./components/welcomePage/WelcomePage";
 import useMessages from "./hooks/useMessage";
 import Advertisements from "./components/advertisements/Advertisements";
-import axios from "axios";
+// import axios from "axios";
 
 const App = () => {
-    const { addMessage } = useMessages();
+  const { addMessage } = useMessages();
 
-    // const [inactive, setInactive] = useState(true);
-    const [sectionFlex, setSectionFlex] = useState({
-        left1: 1,
-        left2: 1,
-        right1: 1,
-        right2: 1,
+  // const [inactive, setInactive] = useState(true);
+  const [sectionFlex, setSectionFlex] = useState({
+    left1: 1,
+    left2: 1,
+    right1: 1,
+    right2: 1,
+  });
+
+  const [playAds, setPlayAds] = useState(true);
+  const [playWelcome, setPlayWelcome] = useState(false);
+  const [sensors, setSensors] = useState([
+    {
+      name: "sensor_1",
+      distance: 175,
+      isSitTaken: false,
+    },
+    {
+      name: "sensor_2",
+      distance: 175,
+      isSitTaken: false,
+    },
+    {
+      name: "sensor_3",
+      distance: 175,
+      isSitTaken: false,
+    },
+    {
+      name: "sensor_4",
+      distance: 175,
+      isSitTaken: false,
+    },
+  ]);
+
+  // const sensors = {
+  //     sensor_1: {
+  //         name: "sensor_1",
+  //         distance: 37,
+  //         isSitTaken: true,
+  //     },
+  //     sensor_2: {
+  //         name: "sensor_2",
+  //         distance: 37,
+  //         isSitTaken: true,
+  //     },
+  //     sensor_3: {
+  //         name: "sensor_3",
+  //         distance: 37,
+  //         isSitTaken: false,
+  //     },
+  //     sensor_4: {
+  //         name: "sensor_4",
+  //         distance: 37,
+  //         isSitTaken: true,
+  //     },
+  // };
+
+  // Control size of left screens using flexbox grow
+  const enlargeLeft = section => {
+    setSectionFlex({
+      ...sectionFlex,
+      left1: section === "left1" ? 7 : 1,
+      left2: section === "left2" ? 7 : 1,
     });
+  };
 
-    const [playAds, setPlayAds] = useState(true);
-    const [playWelcome, setPlayWelcome] = useState(false);
-    const [sensors, setSensors] = useState([
-        {
-            name: "sensor_1",
-            distance: 175,
-            isSitTaken: false,
-        },
-        {
-            name: "sensor_2",
-            distance: 175,
-            isSitTaken: false,
-        },
-        {
-            name: "sensor_3",
-            distance: 175,
-            isSitTaken: false,
-        },
-        {
-            name: "sensor_4",
-            distance: 175,
-            isSitTaken: false,
-        },
-    ]);
+  // Control size of right screens using flexbox grow
+  const enlargeRight = section => {
+    setSectionFlex({
+      ...sectionFlex,
+      right1: section === "right1" ? 7 : 1,
+      right2: section === "right2" ? 7 : 1,
+    });
+  };
 
-    // const sensors = {
-    //     sensor_1: {
-    //         name: "sensor_1",
-    //         distance: 37,
-    //         isSitTaken: true,
-    //     },
-    //     sensor_2: {
-    //         name: "sensor_2",
-    //         distance: 37,
-    //         isSitTaken: true,
-    //     },
-    //     sensor_3: {
-    //         name: "sensor_3",
-    //         distance: 37,
-    //         isSitTaken: false,
-    //     },
-    //     sensor_4: {
-    //         name: "sensor_4",
-    //         distance: 37,
-    //         isSitTaken: true,
-    //     },
-    // };
+  // Reset left screens to default 1:1
+  const resetLeft = () => {
+    setSectionFlex({
+      ...sectionFlex,
+      left1: 1,
+      left2: 1,
+    });
+  };
 
-    // Control size of left screens using flexbox grow
-    const enlargeLeft = (section) => {
-        setSectionFlex({
-            ...sectionFlex,
-            left1: section === "left1" ? 7 : 1,
-            left2: section === "left2" ? 7 : 1,
-        });
-    };
+  // Reset right screens to default 1:1
+  const resetRight = () => {
+    setSectionFlex({
+      ...sectionFlex,
+      right1: 1,
+      right2: 1,
+    });
+  };
 
-    // Control size of right screens using flexbox grow
-    const enlargeRight = (section) => {
-        setSectionFlex({
-            ...sectionFlex,
-            right1: section === "right1" ? 7 : 1,
-            right2: section === "right2" ? 7 : 1,
-        });
-    };
+  useEffect(() => {
+    let timeout;
+    if (sensors.find(el => el.isSitTaken === true)) {
+      setPlayWelcome(true);
+      timeout = setTimeout(() => {
+        setPlayAds(false);
+        // setPlayWelcome(false);
+      }, 2000);
+      setTimeout(() => setPlayWelcome(false), 15000);
+    } else {
+      setPlayAds(true);
+      setPlayWelcome(false);
+    }
 
-    // Reset left screens to default 1:1
-    const resetLeft = () => {
-        setSectionFlex({
-            ...sectionFlex,
-            left1: 1,
-            left2: 1,
-        });
-    };
+    return () => clearTimeout(timeout);
+  }, [sensors]);
 
-    // Reset right screens to default 1:1
-    const resetRight = () => {
-        setSectionFlex({
-            ...sectionFlex,
-            right1: 1,
-            right2: 1,
-        });
-    };
+  // useEffect(() => {
+  //     const fetchSensors = async () => {
+  //         try {
+  //             const res = await axios.get(
+  //                 "http://localhost:3000/sensors/api"
+  //             );
 
-    useEffect(() => {
-        let timeout;
-        if (sensors.find((el) => el.isSitTaken === true)) {
-            setPlayWelcome(true);
-            timeout = setTimeout(() => {
-                setPlayAds(false);
-                // setPlayWelcome(false);
-            }, 2000);
-            setTimeout(() => setPlayWelcome(false), 15000);
-        } else {
-            setPlayAds(true);
-            setPlayWelcome(false);
-        }
+  //             if (res) {
+  //                 setSensors(data.sensors);
+  //             }
+  //         } catch (err) {
+  //             // console.warn(err);
+  //         }
+  //     };
 
-        return () => clearTimeout(timeout);
-    }, [sensors]);
+  //     const intervalId = setInterval(fetchSensors, 100);
+  //     return () => clearInterval(intervalId);
+  // }, []);
 
-    // useEffect(() => {
-    //     const fetchSensors = async () => {
-    //         try {
-    //             const res = await axios.get(
-    //                 "http://localhost:3000/sensors/api"
-    //             );
-
-    //             if (res) {
-    //                 setSensors(data.sensors);
-    //             }
-    //         } catch (err) {
-    //             // console.warn(err);
-    //         }
-    //     };
-
-    //     const intervalId = setInterval(fetchSensors, 100);
-    //     return () => clearInterval(intervalId);
-    // }, []);
-
-    return (
-        <>
-            {playWelcome && <WelcomePage />}
-            {playAds ? (
-                <Advertisements setSensors={setSensors} />
-            ) : (
-                <section className="home">
-                    <section className="home-1">
-                        {sensors[0].isSitTaken && (
-                            <QuarterScreen
-                                id={1}
-                                enlarge={() => enlargeLeft("left1")}
-                                reset={resetLeft}
-                                size={sectionFlex.left1}
-                                isReduced={
-                                    sectionFlex.left1 < sectionFlex.left2 &&
-                                    sectionFlex.left1 < 7
-                                }
-                                isSingle={!sensors[1].isSitTaken}
-                            />
-                        )}
-
-                        {sensors[1].isSitTaken && (
-                            <QuarterScreen
-                                id={2}
-                                enlarge={() => enlargeLeft("left2")}
-                                reset={resetLeft}
-                                size={sectionFlex.left2}
-                                isReduced={
-                                    sectionFlex.left2 < sectionFlex.left1 &&
-                                    sectionFlex.left2 < 7
-                                }
-                                isSingle={!sensors[0].isSitTaken}
-                            />
-                        )}
-                    </section>
-                    <section className="home-2">
-                        {sensors[2].isSitTaken && (
-                            <QuarterScreen
-                                id={3}
-                                enlarge={() => enlargeRight("right1")}
-                                reset={resetRight}
-                                size={sectionFlex.right1}
-                                isReduced={
-                                    sectionFlex.right1 < sectionFlex.right2 &&
-                                    sectionFlex.right1 < 7
-                                }
-                                isSingle={!sensors[3].isSitTaken}
-                            />
-                        )}
-
-                        {sensors[3].isSitTaken && (
-                            <QuarterScreen
-                                id={4}
-                                enlarge={() => enlargeRight("right2")}
-                                reset={resetRight}
-                                size={sectionFlex.right2}
-                                isReduced={
-                                    sectionFlex.right2 < sectionFlex.right1 &&
-                                    sectionFlex.right2 < 7
-                                }
-                                isSingle={!sensors[2].isSitTaken}
-                            />
-                        )}
-                    </section>
-                </section>
+  return (
+    <>
+      {playWelcome && <WelcomePage />}
+      {playAds ? (
+        <Advertisements setSensors={setSensors} />
+      ) : (
+        <section className="home">
+          <section className="home-1">
+            {sensors[0].isSitTaken && (
+              <QuarterScreen
+                id={1}
+                enlarge={() => enlargeLeft("left1")}
+                reset={resetLeft}
+                size={sectionFlex.left1}
+                isReduced={
+                  sectionFlex.left1 < sectionFlex.left2 && sectionFlex.left1 < 7
+                }
+                isSingle={!sensors[1].isSitTaken}
+              />
             )}
-        </>
-    );
+
+            {sensors[1].isSitTaken && (
+              <QuarterScreen
+                id={2}
+                enlarge={() => enlargeLeft("left2")}
+                reset={resetLeft}
+                size={sectionFlex.left2}
+                isReduced={
+                  sectionFlex.left2 < sectionFlex.left1 && sectionFlex.left2 < 7
+                }
+                isSingle={!sensors[0].isSitTaken}
+              />
+            )}
+          </section>
+          <section className="home-2">
+            {sensors[2].isSitTaken && (
+              <QuarterScreen
+                id={3}
+                enlarge={() => enlargeRight("right1")}
+                reset={resetRight}
+                size={sectionFlex.right1}
+                isReduced={
+                  sectionFlex.right1 < sectionFlex.right2 &&
+                  sectionFlex.right1 < 7
+                }
+                isSingle={!sensors[3].isSitTaken}
+              />
+            )}
+
+            {sensors[3].isSitTaken && (
+              <QuarterScreen
+                id={4}
+                enlarge={() => enlargeRight("right2")}
+                reset={resetRight}
+                size={sectionFlex.right2}
+                isReduced={
+                  sectionFlex.right2 < sectionFlex.right1 &&
+                  sectionFlex.right2 < 7
+                }
+                isSingle={!sensors[2].isSitTaken}
+              />
+            )}
+          </section>
+        </section>
+      )}
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/alertMsg/AlertMsg.jsx
@@ -0,0 +1,28 @@
+import styles from "./AlertMsg.module.css";
+import { motion } from "framer-motion";
+const AlertMsg = ({ children, alertType }) => {
+  return (
+    <motion.section
+      initial={{ x: 200, y: 0, opacity: 0 }}
+      animate={{ x: 200, y: -100, opacity: [1, 0], display: "none" }}
+      transition={{
+        opacity: {
+          duration: 3,
+          times: [0, 0.5, 1],
+          ease: "easeInOut",
+        },
+        y: {
+          duration: 2,
+        },
+        display: {
+          delay: 1.5,
+        },
+      }}
+      className={alertType === "success" ? styles.success : styles.failed}
+    >
+      {children}
+    </motion.section>
+  );
+};
+
+export default AlertMsg;
